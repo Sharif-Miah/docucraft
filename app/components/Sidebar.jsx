a15bef7b1b1c08dd58105ab7d2d6eecb87d773bf
Sharif-Miah <farhansharif1410@gmail.com>
2024-12-18 22:14:57 +0600

    dynamic routes in next js code added and one error author is not show

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,15 +1,63 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client';
+
+import {
+  getDocumentsByAuthor,
+  getDocumentsByCategory,
+  getDocumentsByTag,
+} from '@/utils/doc-utils';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 const Sidebar = ({ docs }) => {
-  const roots = docs.filter((doc) => !doc.parent);
-  // console.log({ roots });
+  const pathName = usePathname();
+  const [rootNodes, setRootNodes] = useState([]);
+  const [nonRootNodesGrouped, setNonRootNodesGrouped] = useState({});
 
-  const nonRoots = Object.groupBy(
-    docs.filter((doc) => doc.parent),
-    ({ parent }) => parent
-  );
+  useEffect(() => {
+    let matchedDocs = docs;
 
-  // console.log(nonRoots);
+    if (pathName.includes('/tags')) {
+      const tag = pathName.split('/')[2];
+      matchedDocs = getDocumentsByTag(docs, tag);
+    } else if (pathName.includes('/authors')) {
+      const author = pathName.split('/')[2];
+      matchedDocs = getDocumentsByAuthor(docs, author);
+    } else if (pathName.includes('/categories')) {
+      const category = pathName.split('/')[2];
+      matchedDocs = getDocumentsByCategory(docs, category);
+    }
+
+    const roots = matchedDocs.filter((doc) => !doc.parent);
+
+    const nonRoots = Object.groupBy(
+      matchedDocs.filter((doc) => doc.parent),
+      ({ parent }) => parent
+    );
+
+    const nonRootsKeys = Reflect.ownKeys(nonRoots);
+    nonRootsKeys.forEach((key) => {
+      const foundInRoots = roots.find((root) => root.id === key);
+      if (!foundInRoots) {
+        const foundInDocs = docs.find((doc) => doc.id === key);
+        roots.push(foundInDocs);
+      }
+    });
+
+    roots.sort((a, b) => {
+      if (a.order < b.order) {
+        return -1;
+      }
+      if (a.order > b.order) {
+        return 1;
+      }
+      return 0;
+    });
+
+    setRootNodes([...roots]);
+    setNonRootNodesGrouped({ ...nonRoots });
+  }, [pathName]);
 
   return (
     <nav class='lg:block my-10'>
@@ -22,7 +70,7 @@ const Sidebar = ({ docs }) => {
         <ul
           role='list'
           className='border-l border-transparent'>
-          {roots.map((rootNode) => (
+          {rootNodes.map((rootNode) => (
             <li
               key={rootNode.it}
               className='relative'>
@@ -32,11 +80,11 @@ const Sidebar = ({ docs }) => {
                 href={`/docs/${rootNode.id}`}>
                 <span className='truncate'>{rootNode.title}</span>
               </Link>
-              {nonRoots[rootNode.id] && (
+              {nonRootNodesGrouped[rootNode.id] && (
                 <ul
                   role='list'
                   className='border-l border-transparent'>
-                  {nonRoots[rootNode.id].map((subRoot) => (
+                  {nonRootNodesGrouped[rootNode.id].map((subRoot) => (
                     <li key={subRoot.id}>
                       <Link
                         className='flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white'
